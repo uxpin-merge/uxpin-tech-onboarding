4390b2b7732e0e1595566e82f7713c781d7162a6
Evan Trujillo <evantrujillo30@gmail.com>
2023-06-21 11:53:02 +0900

    refactor: update uxpinwrapper function to use FC

--- src/components/UXPinWrapper/UXPinWrapper.tsx
+++ src/components/UXPinWrapper/UXPinWrapper.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { FC, PropsWithChildren } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import boilerplateTheme from './boilerplate-theme';
 
 const theme = createTheme(boilerplateTheme);
 
-export default function UXPinWrapper({ children }: { children: React.ReactNode }) {
+const UXPinWrapper: FC<PropsWithChildren> = ({ children }) => {
   const icons = document.createElement('link') as HTMLLinkElement;
   icons.setAttribute('rel', 'stylesheet');
   icons.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
   document.head.appendChild(icons);
-
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-}
+};
+
+export default UXPinWrapper;
